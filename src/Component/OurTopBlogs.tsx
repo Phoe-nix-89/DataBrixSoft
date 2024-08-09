9d5e02b11537fa61b8643045a85756c95f96a699
Phoe-nix-89 <sonkhoseth074@gmail.com>
2024-08-09 16:46:25 +0530

    Added a new Section - OurTopBlogs

---
+++ src/Component/OurTopBlogs.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './OurTopBlogs.css';
+import osbtm from '../assets/osbtm.png';
+import ourtop1 from '../assets/ourtop1.png';
+import ourtop2 from '../assets/ourtop2.png';
+
+function OurTopBlogs() {
+  return (
+    <div className='outer'>
+        <div className='tp'>Our Top Blogs</div>
+        <div className='imgdiv'>
+            <img src={osbtm} alt="" />
+        </div>
+        <div className='inner'>
+            <div className='ct'>
+                <a href="" className="bx">
+                    <div className='bl'>
+                        <div>August 09,2024</div>
+                    </div>
+                    <img src={ourtop1} alt="" className="im1" />
+                    <div className='t'>
+                        <div className='mob'>Mobile App Development</div>
+                        <div className='lr'>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, voluptas eligendi assumenda obcaecati quae est?
+                        </div>
+                    </div>
+                </a>
+                <a href="" className="bx">
+                    <div className='bl'>
+                        <div>August 09,2024</div>
+                    </div>
+                    <img src={ourtop2} alt="" className="im1" />
+                    <div className='t'>
+                        <div className='mob'>Mobile App Development</div>
+                        <div className='lr'>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, voluptas eligendi assumenda obcaecati quae est?
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default OurTopBlogs
